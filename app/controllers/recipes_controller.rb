707a465448e1f195fbd2b5d1e4207585084991b9
satub <satu.barnhill@gmail.com>
2016-09-07 16:30:29 -0400

    Working version according to rspec

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,32 @@
+require 'pry'
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
 
   def new
+    #creates a dummy recipe, with 1 dummy ingredient
+    @recipe = Recipe.new
+    ingredient = Ingredient.new
+    @recipe.ingredients << ingredient
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipes_path
   end
 
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids:[], ingredients_attributes: [:name])
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,12 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :recipe_ingredients
 
   validates :name, presence: true
+
+  accepts_nested_attributes_for :ingredients, reject_if: :empty_ingredient_name?
+
+
+  def empty_ingredient_name?(ingred_attr)
+    ingred_attr[:name].empty?
+  end
+  
 end
