405988e62a5d83a49edfa856a1a7e5fc2905416e
fidelscodes <fidelscodes@gmail.com>
2016-09-20 11:30:36 -0400

    add views and controllers for recipes and ingredients

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,22 @@
+class IngredientsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def create
+  end
+end
