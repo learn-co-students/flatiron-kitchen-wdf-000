d769520b64748dd23649abd551266222602f7757
ozPop <ozziepopovas@gmail.com>
2016-09-20 14:01:33 -0400

    add ingredient _form, update new, controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,16 +1,24 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update]
   def index
     @ingredients = Ingredient.search(params[:query])
   end
 
   def new
+    @ingredient = Ingredient.new
   end
 
   def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      redirect_to new_ingredient_path
+    end
+
   end
 
   def show
-    @ingredient = Ingredient.find(params[:id])
   end
 
   def edit
@@ -21,4 +29,15 @@ class IngredientsController < ApplicationController
 
   def delete
   end
+
+  private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
 end
