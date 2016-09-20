2498c3279256d6c906b24ab6a68a6867591f58b7
ozPop <ozziepopovas@gmail.com>
2016-09-20 11:25:15 -0400

    generate recipe controller & views, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ FlatironKitchen::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resource :recipes
   # Example resource route with options:
   #   resources :products do
   #     member do
