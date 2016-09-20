8c98b6a007814b4bfe03ad39220bebcf5f3ef764
fidelscodes <fidelscodes@gmail.com>
2016-09-20 13:23:04 -0400

    add partials for ingredient update create and show actions and views

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,14 +1,17 @@
 class IngredientsController < ApplicationController
   def index
+    @ingredients = Ingredient.all
   end
 
   def show
+    @ingredient = Ingredient.find_by(id: params[:id])
   end
 
   def edit
   end
 
   def new
+    @ingredient = Ingredient.new
   end
 
   def update
@@ -18,5 +21,17 @@ class IngredientsController < ApplicationController
   end
 
   def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
   end
 end
