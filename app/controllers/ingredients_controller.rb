04ffcae5072bf259ca95cbfbae1ac2bad8cd17c6
Bibi Khadiza <limakhadiza@yahoo.com>
2016-10-09 19:52:44 -0400

    done

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,64 @@
+class IngredientsController < ApplicationController
+
+  def new
+  end
+
+
+  def create
+    ingredient = Ingredient.new(ingredient_params)
+    if ingredient.valid?
+      @ingredient = Ingredient.create(ingredient_params)
+      redirect_to ingredient_path(@ingredient)
+    else
+      render "new"
+    end
+  end
+
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.valid?
+      @ingredient.update(ingredient_params)
+      redirect_to ingredient_path(@ingredient)
+    else
+      render "edit"
+    end
+  end
+
+
+
+
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,60 @@
+class RecipesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.valid?
+      @recipe = Recipe.create(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.valid?
+      @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render "edit"
+    end
+  end
+
+
+
+
+
+
+
+  private
+
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids:[])
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
